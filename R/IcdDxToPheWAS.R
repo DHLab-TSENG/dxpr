1c20916d071b161a8e21d6a37faf2bc562326d7a
ChunJuC <52523083+ChunJuC@users.noreply.github.com>
2019-11-05 14:49:48 +0800

    Update IcdDxToPheWAS.R

--- R/IcdDxToPheWAS.R
+++ R/IcdDxToPheWAS.R
@@ -1,11 +1,22 @@
 #' @rdname DxPheWAS
 #' @export
-IcdDxToPheWAS <- function(DxDataFile, idColName, icdColName, dateColName, icd10usingDate, isDescription = TRUE){
+IcdDxToPheWAS <- function(DxDataFile, idColName, icdColName, dateColName, icdVerColName = NULL, icd10usingDate = NULL, isDescription = TRUE){
   DxDataFile <- as.data.table(DxDataFile)
-  DataCol <- c(deparse(substitute(idColName)), deparse(substitute(icdColName)), deparse(substitute(dateColName)))
-  DxDataFile <- DxDataFile[,DataCol,with = FALSE]
-  names(DxDataFile) <- c("ID", "ICD", "Date")
-  Conversion <- IcdDxShortToDecimal(DxDataFile,ICD,Date,icd10usingDate)
+  if(deparse(substitute(icdVerColName)) != "NULL"){
+    DataCol <- c(deparse(substitute(idColName)), deparse(substitute(icdColName)), deparse(substitute(dateColName)), deparse(substitute(icdVerColName)))
+    DxDataFile <- DxDataFile[,DataCol,with = FALSE]
+    names(DxDataFile) <- c("ID", "ICD", "Date", "Version")
+  }else{
+    DataCol <- c(deparse(substitute(idColName)), deparse(substitute(icdColName)), deparse(substitute(dateColName)))
+    DxDataFile <- DxDataFile[,DataCol,with = FALSE]
+    names(DxDataFile) <- c("ID", "ICD", "Date")
+  }
+  if(deparse(substitute(icdVerColName)) != "NULL"){
+    Conversion <- IcdDxShortToDecimal(DxDataFile, ICD, Date, icdVerColName = Version, icd10usingDate = NULL)
+  }else{
+    Conversion <- IcdDxShortToDecimal(DxDataFile, ICD, Date, icdVerColName = NULL, icd10usingDate = icd10usingDate)
+  }
+
   DxDataFile[,c("Date", "Number") := list(as.Date(Date), 1:nrow(DxDataFile))]
   DxDataFile[,Decimal := Conversion$ICD]
 
@@ -14,8 +25,14 @@ IcdDxToPheWAS <- function(DxDataFile, idColName, icdColName, dateColName, icd10u
   }else{
     PheWASCol <- "PheCode"
   }
-  allPheWAS <- rbind(merge(DxDataFile[Date < icd10usingDate,], phecode_icd9_2[,c(PheWASCol,"ICD"), with = FALSE], by.x ="Decimal", by.y = "ICD", all.x = TRUE),
+
+  if (deparse(substitute(icdVerColName)) != "NULL"){
+    allPheWAS <- rbind(merge(DxDataFile[Version == 9,], phecode_icd9_2[,c(PheWASCol,"ICD"), with = FALSE], by.x ="Decimal", by.y = "ICD", all.x = TRUE),
+                       merge(DxDataFile[Version == 10,], phecode_icd9_2[,c(PheWASCol,"ICD"), with = FALSE], by.x ="Decimal", by.y = "ICD", all.x = TRUE))
+  }else{
+    allPheWAS <- rbind(merge(DxDataFile[Date < icd10usingDate,], phecode_icd9_2[,c(PheWASCol,"ICD"), with = FALSE], by.x ="Decimal", by.y = "ICD", all.x = TRUE),
                        merge(DxDataFile[Date >= icd10usingDate,], phecode_icd9_2[,c(PheWASCol,"ICD"), with = FALSE], by.x ="Decimal", by.y = "ICD", all.x = TRUE))
+  }
   allPheWAS <- allPheWAS[order(Number),-"Number"]
 
   if(nrow(allPheWAS[is.na(eval(parse(text = paste(PheWASCol))))]) < nrow(allPheWAS)){
