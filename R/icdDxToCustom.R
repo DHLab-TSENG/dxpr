0c83a5ed1ac169da5593e5d7d7471f8f4a41264c
ChunJuC <52523083+ChunJuC@users.noreply.github.com>
2019-12-10 10:55:30 +0800

    Update and rename IcdDxToCustom.R to icdDxToCustom.R

--- R/icdDxToCustom.R
+++ R/icdDxToCustom.R
@@ -1,15 +1,15 @@
-#' @rdname DxCustom
+#' @rdname dxCustom
 #' @export
 #'
-IcdDxToCustom <- function(DxDataFile, idColName, icdColName, dateColName, CustomGroupingTable){
-  customICD <- as.data.table(DxDataFile)
-  CustomGroupingTable <- as.data.table(CustomGroupingTable)
-  DataCol <- c(deparse(substitute(idColName)), deparse(substitute(icdColName)), deparse(substitute(dateColName)))
-  customICD <- customICD[,DataCol,with = FALSE]
+icdDxToCustom <- function(dxDataFile, idColName, icdColName, dateColName, customGroupingTable){
+  customICD <- as.data.table(dxDataFile)
+  customGroupingTable <- as.data.table(customGroupingTable)
+  dataCol <- c(deparse(substitute(idColName)), deparse(substitute(icdColName)), deparse(substitute(dateColName)))
+  customICD <- customICD[,dataCol,with = FALSE]
   names(customICD) <- c("ID", "ICD", "Date")
   customICD[,c("Date", "Number") := list(as.Date(Date), 1:nrow(customICD))]
 
-  groupedICD <- merge(customICD, CustomGroupingTable, by = "ICD", all.x = TRUE)[order(Number), -"Number"]
+  groupedICD <- merge(customICD, customGroupingTable, by = "ICD", all.x = TRUE)[order(Number), -"Number"]
 
   if(sum(!is.na(groupedICD$Group)) > 0){
     summarisedgroupedICD <- groupedICD[!is.na(Group),
