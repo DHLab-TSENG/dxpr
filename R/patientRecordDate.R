4395efc476d80de1d7caae76b041e0966ef203e1
ChunJuC <52523083+ChunJuC@users.noreply.github.com>
2020-04-16 15:59:59 +0800

    Update patientRecordDate.R

--- R/patientRecordDate.R
+++ R/patientRecordDate.R
@@ -1,11 +1,11 @@
 #' @rdname recordPeriod
 #' @export
 #'
-patientRecordDate <- function(dxDataFile, idColName, icdColName, dateColName){
+patientRecordDate <- function(dxDataFile, idColName, dateColName){
   dxDataFile <- as.data.table(dxDataFile)
-  DataCol <- c(deparse(substitute(idColName)), deparse(substitute(icdColName)), deparse(substitute(dateColName)))
+  DataCol <- c(deparse(substitute(idColName)), deparse(substitute(dateColName)))
   dxDataFile <- dxDataFile[,DataCol,with = FALSE]
-  names(dxDataFile) <- c("ID", "ICD", "Date")
+  names(dxDataFile) <- c("ID", "Date")
   dxDataFile[,"Date"] <- as.Date(dxDataFile[,Date])
 
   recordDate <- dxDataFile[,list(firstRecordDate = min(Date), endRecordDate = max(Date)),by = "ID"][order(firstRecordDate,endRecordDate),]
