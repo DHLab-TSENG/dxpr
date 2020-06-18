5369675fa0e3898d2e1b94d9af384e7ac228889a
ChunJuC <52523083+ChunJuC@users.noreply.github.com>
2020-06-18 14:14:09 +0800

    Update icdDxShortToDecimal.R

--- R/icdDxShortToDecimal.R
+++ R/icdDxShortToDecimal.R
@@ -11,10 +11,11 @@ icdDxShortToDecimal <- function(dxDataFile, icdColName, dateColName, icdVerColNa
     dataCol <- c(deparse(substitute(icdColName)), deparse(substitute(dateColName)))
     dxDataFile <- setDT(dxDataFile)[,dataCol,with = FALSE]
     names(dxDataFile) <- c("ICD", "Date")
+    ifelse(is.na(dxDataFile$Version), stop("NA is detected. Please make sure all values in ICD version column are non-null."),dxDataFile$Version)
   }
 
   dxDataFile[,c("Date", "Number") := list(as.Date(format(Date)), 1:nrow(dxDataFile))]
-  ifelse(is.na(dxDataFile$Date), stop("NA is detected. Please make sure all values in ICD column are non-null and in the correct date format."),dxDataFile$Date)
+  ifelse(is.na(dxDataFile$Date), stop("NA is detected. Please make sure all values in Date column are non-null and in the correct date format."),dxDataFile$Date)
   ifelse(is.na(dxDataFile$ICD), stop("NA is detected. Please make sure all values in ICD column are non-null."),dxDataFile$ICD)
 
   icd_Decimal <- dxDataFile[grepl("[.]", dxDataFile$ICD),]
