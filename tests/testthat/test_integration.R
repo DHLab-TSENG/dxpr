cdec477b1d354ed1c862e48df14d41911d736269
ChunJuC <ju.chin.0902@gmail.com>
2021-04-03 13:42:50 +0800

    test files update

--- tests/testthat/test_integration.R
+++ tests/testthat/test_integration.R
@@ -1,12 +1,15 @@
 context("integration")
 #icdStD
 df1 <- data.table("ICD" = "A0.11", "count" = 20, "IcdVersionInFile" = "ICD 10", "WrongType" = "Wrong format", "Suggestion" = "")
+df1_t <- data.table("ICD" = "7552", "count" = 2, "IcdVersionInFile" = "ICD 9", "WrongType" = "Wrong format", "Suggestion" = "75529")
+
 
 auto_df1 <- icdDxShortToDecimal(sampleDxFile, ICD, Date, icd10usingDate = "2015/10/01")
 
 test_that("ICD short -> decimal Error",
           { expect_equal(auto_df1$Error[1], df1)})
-
+test_that("ICD short -> decimal Error",
+          { expect_equal(auto_df1$Error[16], df1_t)})
 test_that("ICD short -> decimal Error",
           {expect_output(str(auto_df1$Error), "16 obs")})
 
@@ -16,12 +19,14 @@ test_that("ICD short -> decimal Type",
 
 #icdDtS
 df2 <- data.table("ICD" = "A0.11", "count" = 20, "IcdVersionInFile" = "ICD 10", "WrongType" = "Wrong format", "Suggestion" = "")
+df2_t <- data.table("ICD" = "7552", "count" = 2, "IcdVersionInFile" = "ICD 9", "WrongType" = "Wrong format", "Suggestion" = "75529")
 
 auto_df2 <- icdDxDecimalToShort(sampleDxFile, ICD, Date, icd10usingDate = "2015/10/01")
 
 test_that("ICD decimal -> short Error",
           { expect_equal(auto_df2$Error[1], df2)})
-
+test_that("ICD short -> decimal Error",
+          { expect_equal(auto_df2$Error[16], df2_t)})
 test_that("ICD decimal -> short Error",
           {expect_output(str(auto_df2$Error), "16 obs")})
 
@@ -31,26 +36,53 @@ test_that("ICD decimal -> short Type",
 
 #icdCCS
 df3 <- data.table("ID" = "A0", "CCS_CATEGORY_DESCRIPTION" = "Chronic kidney disease", "firstCaseDate" = as.Date("2009-07-25"), "endCaseDate" = as.Date("2013-12-20"), "count" = 5, "period" = as.difftime(1609, units = "days"))
+df3_t <- data.table("ID" = "D8", "CCS_CATEGORY_DESCRIPTION" = "Schizophrenia and other psychotic disorders", "firstCaseDate" = as.Date("2007-01-30"), "endCaseDate" = as.Date("2007-01-30"), "count" = 1, "period" = as.difftime(0, units = "days"))
 
 auto_df3 <- icdDxToCCS(sampleDxFile, ID, ICD, Date, icd10usingDate =  "2015-10-01", isDescription = TRUE)
 
 test_that("ICD CCS",
           { expect_equal(auto_df3$summarised_groupedDT[1], df3)})
+test_that("ICD CCS",
+          { expect_equal(auto_df3$summarised_groupedDT[97], df3_t)})
+
+test_that("ICD CCS",
+          { expect_output(str(auto_df3$summarised_groupedDT), "97 obs")})
+test_that("ICD CCS",
+          { expect_output(str(auto_df3$groupedDT), "300 obs")})
+test_that("ICD CCS",
+          { expect_output(str(auto_df3$groupedDT), "5 variables")})
 
 
 #icdComorbid
 df4 <- data.table("ID" = "A0", "Comorbidity" = "RD", "firstCaseDate" = as.Date("2009-07-25"), "endCaseDate" = as.Date("2013-12-20"), "count" = 5, "period" = as.difftime(1609, units = "days"))
+df4_t <- data.table("ID" = "D8", "Comorbidity" = "PARA", "firstCaseDate" = as.Date("2010-09-27"), "endCaseDate" = as.Date("2010-09-27"), "count" = 1, "period" = as.difftime(0, units = "days"))
 
 auto_df4 <- icdDxToComorbid(sampleDxFile, ID, ICD, Date, icdVerColName = NULL, "2015-10-01", charlson)
 
 test_that("ICD Comorbid",
           { expect_equal(auto_df4$summarised_groupedDT[1], df4)})
-
+test_that("ICD Comorbid",
+          { expect_equal(auto_df4$summarised_groupedDT[51], df4_t)})
+test_that("ICD Comorbid",
+          { expect_output(str(auto_df4$summarised_groupedDT), "51 obs")})
+test_that("ICD Comorbid",
+          { expect_output(str(auto_df4$groupedDT), "300 obs")})
+test_that("ICD Comorbid",
+          { expect_output(str(auto_df4$groupedDT), "5 variables")})
 
 #icdPheWAS
 df5 <- data.table("ID" = "A2", "PheCode" = "585.31", "firstCaseDate" = as.Date("2015-08-12"), "endCaseDate" = as.Date("2020-05-22"), "count" = 2, "period" = as.difftime(1745, units = "days"))
+df5_t <- data.table("ID" = "C2", "PheCode" = "300.3", "firstCaseDate" = as.Date("2020-03-24"), "endCaseDate" = as.Date("2020-03-24"), "count" = 1, "period" = as.difftime(0, units = "days"))
 
 auto_df5 <- icdDxToPheWAS(sampleDxFile, ID, ICD, Date, icdVerColName = NULL, "2015-10-01", FALSE)
 
 test_that("ICD PheWAS",
           { expect_equal(auto_df5$summarised_groupedDT[1], df5)})
+test_that("ICD PheWAS",
+          { expect_equal(auto_df5$summarised_groupedDT[153], df5_t)})
+test_that("ICD PheWAS",
+          { expect_output(str(auto_df5$summarised_groupedDT), "153 obs")})
+test_that("ICD PheWAS",
+          { expect_output(str(auto_df5$groupedDT), "304 obs")})
+test_that("ICD PheWAS",
+          { expect_output(str(auto_df5$groupedDT), "5 variables")})
--- tests/testthat/test_wrangling.R
+++ tests/testthat/test_wrangling.R
@@ -11,15 +11,26 @@ sc <- selectCases(dxDataFile = sampleDxFile,
 
 test_that("Select Cases",
           { expect_equal(sum(sc$selectedCase == "Selected"), 24L)})
+test_that("Select Cases",
+          { expect_output(str(sc), "38 obs")})
+test_that("Select Cases",
+          { expect_output(str(sc), "8 variables")})
 
 
 #conditionEra
 df6 <- data.table("ID" = "A0", "CCS_LVL_3_LABEL" = "Chronic kidney disease", "era" = 1L,  "firstCaseDate" = as.Date("2009-07-25"), "endCaseDate" = as.Date("2009-07-25"), "count" = 1, "period" = as.difftime(0, units = "days"))
+df6_t <- data.table("ID" = "D8", "CCS_LVL_3_LABEL" = "Schizophrenia  other psychotic disorders", "era" = 1L,  "firstCaseDate" = as.Date("2007-01-30"), "endCaseDate" = as.Date("2007-01-30"), "count" = 1, "period" = as.difftime(0, units = "days"))
 
 era <- getConditionEra(sampleDxFile, ID, ICD, Date, icdVerColName = NULL, "2015-10-01",
                        groupDataType = CCSlvl3)
 test_that("Condition Era",
           { expect_equal(era[1], df6)})
+test_that("Condition Era",
+          { expect_equal(era[98], df6_t)})
+test_that("Condition Era",
+          { expect_output(str(era), "98 obs")})
+test_that("Condition Era",
+          { expect_output(str(era), "7 variables")})
 
 
 #period
@@ -27,6 +38,10 @@ record <- getEligiblePeriod(sampleDxFile, ID, Date)
 
 test_that("Period",
           { expect_equal(record$endRecordDate[1] - record$firstRecordDate[1], as.difftime(7028, units = "days"))})
+test_that("Period",
+          { expect_output(str(record), "38 obs")})
+test_that("Period",
+          { expect_output(str(record), "3 variables")})
 
 
 #split
@@ -35,9 +50,17 @@ record$endRecordDate <- NULL
 splitedData <- splitDataByDate(sampleDxFile, ID, ICD, Date, record, gap = 30)
 
 df7 <- data.table("ID" = "A0", "ICD" = "5856", "Date" = as.Date("2009-07-25"), "indexDate" = as.Date("2009-07-25"), "timeTag" = "A", "window" = 1)
+df7_t <- data.table("ID" = "D8", "ICD" = "C8582", "Date" = as.Date("2025-09-10"), "indexDate" = as.Date("2007-01-30"), "timeTag" = "A", "window" = 227)
 
 test_that("Split",
           { expect_equal(splitedData[1], df7)})
+test_that("Split",
+          { expect_equal(splitedData[300], df7_t)})
+test_that("Split",
+          { expect_output(str(splitedData), "300 obs")})
+test_that("Split",
+          { expect_output(str(splitedData), "6 variables")})
+
 
 #wideFormat
 auto_df4 <- icdDxToComorbid(sampleDxFile, ID, ICD, Date, icdVerColName = NULL, "2015-10-01", charlson)
@@ -47,5 +70,7 @@ groupedDataWide <- groupedDataLongToWide(auto_df4$groupedDT,
                                          categoryColName = Comorbidity,
                                          dateColName = Date)
 
+test_that("Wide Format",
+          {expect_output(str(groupedDataWide), "38 obs")})
 test_that("Wide Format",
           {expect_output(str(groupedDataWide), "11 variables")})
